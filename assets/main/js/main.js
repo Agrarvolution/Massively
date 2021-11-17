e8a1422f687b254cbbd4f0398a346a8aa667485b
Agrarvolution <kilian.hartinger@aon.at>
2021-11-17 12:16:18 +0100

    Remove unused code

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -53,25 +53,7 @@
 		$this.each(function() {
 
 			var $t = $(this),
-				$bg = $('<div class="bg"></div>').appendTo($t),
-				on, off;
-
-			on = function() {
-
-				$bg
-					.removeClass('fixed')
-					.css('transform', 'matrix(1,0,0,1,0,0)');
-
-				$window
-					.on('scroll._parallax', function() {
-
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
-
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
-
-					});
-
-			};
+				$bg = $('<div class="bg"></div>').appendTo($t), off;
 
 			off = function() {
 
@@ -84,21 +66,7 @@
 
 			};
 
-			/*// Disable parallax on ..
-				if (browser.name == 'ie'			// IE
-				||	browser.name == 'edge'			// Edge
-				||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
-				||	browser.mobile)					// Mobile devices*/
-					off();
-
-			// Enable everywhere else.
-			/*
-				else {
-
-					breakpoints.on('>large', on);
-					breakpoints.on('<=large', off);
-
-				}*/
+			off();
 		});
 
 		$window
