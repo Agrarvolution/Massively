f338ef70a04cf0327115b7da6d033ed01f0513c0
Agrarvolution <kilian.hartinger@aon.at>
2022-05-07 21:18:19 +0200

    Remove Jquery and Scrollex from main.js

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -5,17 +5,12 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function ($) {
-
-	var $window = $(window),
-		$body = $('body'),
-		$wrapper = $('#wrapper');
-	const NAV = document.querySelector('#nav');
-	const WRAPPER = document.querySelector('#wrapper');
-	const NAV_PANEL_TOGGLE = document.querySelector('#navPanelToggle');
-	const NAV_CONTENT = document.querySelectorAll('#nav > *');
-	const BODY = document.querySelector('body');
-	const NAV_PANEL = document.querySelector('#navPanel > nav');
+(() => {
+	const Nav = document.querySelector('#nav');
+	const NavPanelToggle = document.querySelector('#navPanelToggle');
+	const NavContent = document.querySelectorAll('#nav > *');
+	const Body = document.querySelector('body');
+	const NavPanel = document.querySelector('#navPanel > nav');
 
 	// Breakpoints.
 	var breakPointSource = {
@@ -30,77 +25,21 @@
 	}
 	breakpoints(breakPointSource);
 
-	/**
-	 * Applies parallax scrolling to an element's background image.
-	 * @return {jQuery} jQuery object.
-	 */
-	$.fn._parallax = function (intensity) {
-		var $window = $(window),
-			$this = $(this);
-
-		if (this.length == 0 || intensity === 0)
-			return $this;
-
-		if (this.length > 1) {
-
-			for (var i = 0; i < this.length; i++)
-				$(this[i])._parallax(intensity);
-
-			return $this;
-
-		}
-
-		if (!intensity)
-			intensity = 0.1;
-
-		$this.each(function () {
-
-			var $t = $(this),
-				$bg = $('<div class="bg"></div>').appendTo($t), off;
-
-			off = function () {
-
-				$bg
-					.addClass('fixed')
-					.css('transform', 'none');
-
-				$window
-					.off('scroll._parallax');
-
-			};
-
-			off();
-		});
-
-		$window
-			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function () {
-				$window.trigger('scroll');
-			});
-
-		return $(this);
-
-	};
-
-	// Play initial animations on page load.
-	$window.on('load', function () {
+	// // Play initial animations on page load.
+	window.addEventListener('load', event => {
 		window.setTimeout(function () {
-			$body.removeClass('is-preload');
-		}, 100);
+			Body.classList.remove('is-preload');
+		}, 50);
 	});
-
-
-	// Background.
-	$wrapper._parallax(0.925);
 
 	// Nav Panel switch between portrait and landscape
 	breakpoints.on('>medium', function () {
 
 		// NavPanel -> Nav.
-		NAV.replaceChildren(...NAV_CONTENT);
+		Nav.replaceChildren(...NavContent);
 
 		// Flip icon classes.
-		NAV.querySelectorAll('.icons, .icon').forEach(node => {
+		Nav.querySelectorAll('.icons, .icon').forEach(node => {
 			node.classList.remove('alt');
 		});
 	});
@@ -108,20 +47,20 @@
 	breakpoints.on('<=medium', function () {
 
 		// Nav -> NavPanel.
-		NAV_PANEL.replaceChildren(...NAV_CONTENT);
+		NavPanel.replaceChildren(...NavContent);
 
 		// Flip icon classes.
-		NAV_PANEL.querySelectorAll('.icons, .icon').forEach(node => {
+		NavPanel.querySelectorAll('.icons, .icon').forEach(node => {
 			node.classList.add('alt');
 		})
 	});
 
-	NAV_PANEL_TOGGLE.addEventListener('click', event => {
-		BODY.classList.add('is-navPanel-visible');
+	NavPanelToggle.addEventListener('click', event => {
+		Body.classList.add('is-navPanel-visible');
 		return true;
 	});
 	document.querySelector('#navPanel a.close').addEventListener('click', event => {
-		BODY.classList.remove('is-navPanel-visible');
+		Body.classList.remove('is-navPanel-visible');
 		return true;
 	})
 
@@ -136,37 +75,36 @@
 			return console.warn('Browser does not support IntersectionObserver.');
 		}
 
-		const SCROLL_TIMEOUT = 500; //ms
-		const INTRO = document.querySelector('#intro');
-		const MAIN = document.querySelector('#main');
+		const ScrollTimeout = 500; //ms
+		const Intro = document.querySelector('#intro');
 
-		const LOGO_IMAGE = document.querySelector('#header img')
+		const LogoImage = document.querySelector('#header img')
 		altMenuObserver();
 		fadeInObserver();
 
 
 		function altMenuObserver() {
-			if (!LOGO_IMAGE) {
+			if (!LogoImage) {
 				return false;
 			}
 			let menuObserver = new IntersectionObserver(entries => {
 				console.log(entries[0]);
 				if (entries[0].isIntersecting) {
-					NAV.classList.remove('alt');
-					NAV_PANEL_TOGGLE.classList.remove('alt');
+					Nav.classList.remove('alt');
+					NavPanelToggle.classList.remove('alt');
 				} else {
-					NAV.classList.add('alt');
-					NAV_PANEL_TOGGLE.classList.add('alt');
+					Nav.classList.add('alt');
+					NavPanelToggle.classList.add('alt');
 				}
 			}, {
 				rootMargin: '0px',
 				threshold: 0
 			});
-			menuObserver.observe(LOGO_IMAGE);
+			menuObserver.observe(LogoImage);
 		}
 
 		function fadeInObserver() {
-			if (!INTRO) {
+			if (!Intro) {
 				return false;
 			}
 			let previousY = 0, previousTime = 0;
@@ -174,15 +112,15 @@
 				if (entries[0].boundingClientRect.y >= 0) {
 					return false;
 				}
-				if ((entries[0].time - previousTime) < SCROLL_TIMEOUT && Math.floor((previousY - entries[0].boundingClientRect.y) / 20) === 0) {
+				if ((entries[0].time - previousTime) < ScrollTimeout && Math.floor((previousY - entries[0].boundingClientRect.y) / 20) === 0) {
 					previousTime = entries[0].time;
 					return false;
 				}
 
 				if (entries[0].isIntersecting) {
-					INTRO.classList.remove('hidden');
+					Intro.classList.remove('hidden');
 				} else {
-					INTRO.classList.add('hidden');
+					Intro.classList.add('hidden');
 				}
 				previousY = entries[0].boundingClientRect.y;
 				previousTime = entries[0].time;
@@ -191,7 +129,7 @@
 				rootMargin: '0px',
 				threshold: 0.5
 			});
-			introObserver.observe(INTRO);
+			introObserver.observe(Intro);
 		}
 	}
 
@@ -218,25 +156,76 @@
 	}
 
 
-	//make figure images clickable
-	var lastSeen = {};
-	$('body').on('click', 'img', function () {
-		var $this = $(this);
 
-		if (this.hasAttribute('data-link')) {
-			if ($this.parents('.kg-gallery-container').is(':not(.kg-gallery-container--narrow)') || lastSeen === this) {
-				console.log($this.is(':focus'), $this.parents('.kg-gallery-container').is(':not(.kg-gallery-container--narrow)'), $this.parents('.kg-gallery-container'));
-				window.open(this.getAttribute('data-link'));
-			} else if ($this.parents('.kg-gallery-container').is('.kg-gallery-container--narrow')) {
-				lastSeen = this;
+	/** Clickable images helper methods */
+	function isLinkOnsite(href) {
+		if (href === undefined || href == null || href === '') {
+			return false;
+		}
+
+		const fileLink = href.match(/^\/.+/g);
+		if (fileLink) {
+			return true;
+		}
+
+		try {
+			var url = new URL(href);
+			if (location.host === url.host) {
+				return true;
 			}
 		}
+		catch (e) {
+			console.log(e)
+		}
+
+		return false;
+	}
+	function createSubLink(href, size) {
+		if (size && size.match(/^\d+$/g)) {
+			size = `/w${size}/`;
+		} else {
+			size = '';
+		}
+
+		return replaceFolder(href, size);
+
+		function replaceFolder (href, size) {
+			const HasFolder = href.match(/\/w\d+\//g);
+			if (HasFolder && size === '') {
+				return href.replace(/\/images\/size\/w\d+\//g, '/images/');
+			}
+			if (HasFolder) {
+				href.replace(/\/w\d+\//g, size);
+			}
+			return href.replace('/images/', '/images/size' + size);
+		}
+	}
+
+	const Images = document.querySelectorAll('img');
+	for (let i = 0; i < Images.length; i++) {
+		let fullSrc = Images[i].src;
+		if (isLinkOnsite(fullSrc)) {
+			fullSrc = createSubLink(Images[i].src, '');
+		}
+		if (Images[i].parentNode.tagName !== 'A') {
+			Images[i].setAttribute('data-link', fullSrc);
+		}
+	}
+	//make figure images clickable
+	Body.addEventListener('click', (event) => {
+		if (!event.target.hasAttribute('data-link')) {
+			return true;
+		}
+		if (event.target.closest('.kg-image-card, .kg-gallery-card')) {
+			window.open(event.target.getAttribute('data-link'));
+		}
+		return true;
 	})
 
 	/**
 	 * Viewport width without scrollbar variable for css
 	 */
-	let scroller = document.scrollingElement;
+	const scroller = document.scrollingElement;
 
 	// Force scrollbars to display
 	scroller.style.setProperty('overflow', 'scroll');
@@ -256,8 +245,6 @@
 					'--viewport-width',
 					scroller.clientWidth + "px"
 				);
-
-			// Width of the scrollbar
 			scroller.style
 				.setProperty(
 					'--scrollbar-width',
@@ -273,7 +260,7 @@
 		});
 		return true;
 	}
-	$(window).on('resize', updateScrollbarCSS);
+	window.addEventListener('resize', updateScrollbarCSS);
 	updateScrollbarCSS();
 
-})(jQuery);
+})();
